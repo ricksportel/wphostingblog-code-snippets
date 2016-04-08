ebf3e1efe731df1e92c8ced7de291a1dc48c25be
Rick Sportel <ricksportel@gmail.com>
2016-04-08 10:40:13 +0200

    Update add-hsts-header.php

--- articles/wordpress-website-sneller-maken/add-hsts-header.php
+++ articles/wordpress-website-sneller-maken/add-hsts-header.php
@@ -1,14 +1,16 @@
 <?php
 
-// Add the code snippet below to your WordPress (child) theme's functions.php
+//* Add the code snippet below to your WordPress child theme's functions.php
 
 
-add_action( 'send_headers', 'child_themename_strict_transport_security_header' );
+add_action( 'send_headers', 'child_wphostingblog_strict_transport_security_header' );
 /**
  * Enables the HTTP Strict Transport Security (HSTS) header.
+ *
+ * @since 1.0.0
  */
-function child_themename_strict_transport_security_header() {
- 
-    header( 'Strict-Transport-Security: max-age=10886400; includeSubDomains; preload' );
- 
+function child_wphostingblog_strict_transport_security_header() {
+
+	header( 'Strict-Transport-Security: max-age=10886400; includeSubDomains; preload' );
+
 }
